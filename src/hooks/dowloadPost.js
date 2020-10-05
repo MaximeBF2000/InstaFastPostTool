1d92e662c2588a748ba220727b34455914710bdd
Maxime BOUNAAS-FERRET <maxime.bounaas@gmail.com>
2020-10-06 00:11:24 +0200

    small fix

--- src/hooks/dowloadPost.js
+++ src/hooks/dowloadPost.js
@@ -2,7 +2,7 @@ import html2canvas from 'html2canvas'
 import { canvas } from "../app"
 
 export default function downloadPost(downloadButton) {
-  html2canvas(canvas, { allowTaint: true })
+  html2canvas(canvas, { allowTaint: true, useCORS: true })
     .then(canva => {
       const image = canva.toDataURL("image/jpg")
       downloadButton.href = image
